91430d41a59b439db655882b7ac47b55bd45065a
Mikhail Timoshin <mytimo10@mts.ru>
2024-08-03 23:07:16 +0300

    refactor: optimization code

--- dist/react-composable-api.js
+++ dist/react-composable-api.js
@@ -4,86 +4,72 @@
  * @license MIT
  * @link https://react-composable.com
  **/
-import { useRef as c, useEffect as s, useState as d, useMemo as p } from "react";
-function v(e) {
-  const n = new CustomEvent(e.name, {
-    detail: {
-      payload: e.payload
-    }
-  }), r = () => document.dispatchEvent(n);
-  return {
-    name: e.name,
-    event: n,
-    dispatch: r
-  };
+import { useRef as f, useEffect as s, useState as d, useMemo as p } from "react";
+function m({ name: e, payload: t }) {
+  const n = new CustomEvent(e, { detail: { payload: t } });
+  return { name: e, event: n, dispatch: () => document.dispatchEvent(n) };
 }
-function w(e, n) {
-  const r = typeof e, f = {
-    set(t, u, o) {
-      return u === "value" ? (t[u] = o, n(t.value), !0) : !1;
+function i(e, t) {
+  const n = typeof e, c = {
+    set(r, o, u) {
+      return o === "value" ? (r[o] = u, t(r.value), !0) : !1;
     }
-  }, i = {
-    set(t, u, o) {
-      return t[u] = o, n(Array.isArray(t) ? [...t] : { ...t }), !0;
+  }, l = {
+    set(r, o, u) {
+      return r[o] = u, t(Array.isArray(r) ? [...r] : { ...r }), !0;
     },
-    deleteProperty(t, u) {
-      return delete t[u], n(Array.isArray(t) ? [...t] : { ...t }), !0;
+    deleteProperty(r, o) {
+      return delete r[o], t(Array.isArray(r) ? [...r] : { ...r }), !0;
     }
-  }, m = {
-    apply(t, u, o) {
-      return t.apply(u, o);
+  }, y = {
+    apply(r, o, u) {
+      return r.apply(o, u);
     }
   };
-  if (r === "string" || r === "boolean" || r === "symbol") {
-    const t = { value: e };
-    return new Proxy(t, f);
-  } else if (r === "number") {
-    const t = { value: e };
-    return new Proxy(t, {
-      ...f,
-      set(u, o, l) {
-        return o === "value" ? typeof l == "number" ? (u[o] = l, n(u.value), !0) : (console.warn("New value must be a number"), !1) : !1;
+  if (n === "string" || n === "boolean" || n === "symbol")
+    return new Proxy({ value: e }, c);
+  if (n === "number")
+    return new Proxy({ value: e }, {
+      ...c,
+      set(r, o, u) {
+        return o === "value" && typeof u == "number" ? (r[o] = u, t(r.value), !0) : (console.warn("New value must be a number"), !1);
       }
     });
-  } else {
-    if (Array.isArray(e))
-      return new Proxy(e, i);
-    if (r === "object" && e !== null)
-      return new Proxy(e, i);
-    if (r === "function")
-      return new Proxy(e, m);
-    throw new Error("Unsupported data type");
-  }
-}
-function h(e) {
-  const [n, r] = d(e);
-  return p(() => w(n, r), [n]);
-}
-function M(e, n) {
-  Array.isArray(e) || (e = [e]);
-  const r = c(!1);
-  s(() => {
-    if (!r.current) {
-      r.current = !0;
-      return;
-    }
-    n();
-  }, e);
-}
-function x(e) {
-  const n = c(!1);
-  s(() => {
-    n.current || (e(), n.current = !0);
-  }, [e]);
+  if (Array.isArray(e) || n === "object" && e !== null)
+    return new Proxy(e, l);
+  if (n === "function")
+    return new Proxy(e, y);
+  throw new Error("Unsupported data type");
 }
 function E(e) {
-  const n = c(!1), r = c(!1);
+  const [t, n] = d(e);
+  return p(() => i(t, n), [t]);
+}
+function w(e, t) {
+  Array.isArray(e) || (e = [e]);
+  const n = f(!1);
   s(() => {
     if (!n.current) {
       n.current = !0;
       return;
     }
-    r.current || (e(), r.current = !0);
+    t();
+  }, e);
+}
+function b(e) {
+  const t = f(!1);
+  s(() => {
+    t.current || (e(), t.current = !0);
+  }, [e]);
+}
+function h(e) {
+  const t = f(!1), n = f(!1);
+  s(() => {
+    if (!t.current) {
+      t.current = !0;
+      return;
+    }
+    n.current || (e(), n.current = !0);
   }, [e]);
 }
 function A(e) {
@@ -91,44 +77,46 @@ function A(e) {
     e();
   });
 }
-function P(e) {
-  const n = c(!1);
+function M(e) {
+  const t = f(!1);
   s(() => () => {
-    n.current ? e() : n.current = !0;
+    t.current ? e() : t.current = !0;
   }, []);
 }
-function U(e, n) {
-  const r = (f) => n(f.detail.payload);
-  s(() => (document.addEventListener(e, r), () => {
-    document.removeEventListener(e, r);
-  }), []);
+function P(e, t) {
+  s(() => {
+    const n = (c) => t(c.detail.payload);
+    return document.addEventListener(e, n), () => {
+      document.removeEventListener(e, n);
+    };
+  }, []);
 }
-function H(e) {
-  return a(e), h(e);
+function v(e) {
+  return a(e), E(e);
 }
 function a(e) {
   if (typeof e == "object" && e !== null)
-    for (let n in e)
-      e.hasOwnProperty(n) && (typeof e[n] == "object" && e[n] !== null ? a(e[n]) : e[n] = y(e[n]));
+    for (const t in e)
+      e.hasOwnProperty(t) && (typeof e[t] == "object" && e[t] !== null ? a(e[t]) : e[t] = i(e[t]));
 }
-const y = H, R = M, b = x, B = E, C = A, I = P, L = U, O = {
-  ref: y,
-  createEvent: v,
-  onEvent: L,
-  watch: R,
-  onBeforeMount: b,
-  onMounted: B,
-  onUpdate: C,
-  onUnmount: I
+const x = v, U = w, j = b, H = h, R = A, S = M, B = P, O = {
+  ref: x,
+  createEvent: m,
+  onEvent: B,
+  watch: U,
+  onBeforeMount: j,
+  onMounted: H,
+  onUpdate: R,
+  onUnmount: S
 };
 export {
-  v as createEvent,
+  m as createEvent,
   O as default,
-  b as onBeforeMount,
-  L as onEvent,
-  B as onMounted,
-  I as onUnmount,
-  C as onUpdate,
-  y as ref,
-  R as watch
+  j as onBeforeMount,
+  B as onEvent,
+  H as onMounted,
+  S as onUnmount,
+  R as onUpdate,
+  x as ref,
+  U as watch
 };
